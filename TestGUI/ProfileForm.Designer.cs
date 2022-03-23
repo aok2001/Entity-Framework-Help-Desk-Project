afef3cf41b289be776d3eb2cfc563af8a974800c
aok2001 <karpiuk2001@gmail.com>
2022-03-23 10:09:39 -0600

    Created Profile Form

---
+++ TestGUI/ProfileForm.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace TestGUI
+{
+    partial class ProfileForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblName = new System.Windows.Forms.Label();
+            this.lblOccupation = new System.Windows.Forms.Label();
+            this.lblBirthDate = new System.Windows.Forms.Label();
+            this.btnDone = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblName.Location = new System.Drawing.Point(12, 9);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(52, 21);
+            this.lblName.TabIndex = 0;
+            this.lblName.Text = "label1";
+            // 
+            // lblOccupation
+            // 
+            this.lblOccupation.AutoSize = true;
+            this.lblOccupation.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblOccupation.Location = new System.Drawing.Point(12, 30);
+            this.lblOccupation.Name = "lblOccupation";
+            this.lblOccupation.Size = new System.Drawing.Size(38, 15);
+            this.lblOccupation.TabIndex = 1;
+            this.lblOccupation.Text = "label1";
+            // 
+            // lblBirthDate
+            // 
+            this.lblBirthDate.AutoSize = true;
+            this.lblBirthDate.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblBirthDate.Location = new System.Drawing.Point(12, 89);
+            this.lblBirthDate.Name = "lblBirthDate";
+            this.lblBirthDate.Size = new System.Drawing.Size(38, 15);
+            this.lblBirthDate.TabIndex = 2;
+            this.lblBirthDate.Text = "label1";
+            // 
+            // btnDone
+            // 
+            this.btnDone.Location = new System.Drawing.Point(12, 107);
+            this.btnDone.Name = "btnDone";
+            this.btnDone.Size = new System.Drawing.Size(194, 23);
+            this.btnDone.TabIndex = 3;
+            this.btnDone.Text = "Done";
+            this.btnDone.UseVisualStyleBackColor = true;
+            this.btnDone.Click += new System.EventHandler(this.btnDone_Click);
+            // 
+            // ProfileForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(218, 142);
+            this.Controls.Add(this.btnDone);
+            this.Controls.Add(this.lblBirthDate);
+            this.Controls.Add(this.lblOccupation);
+            this.Controls.Add(this.lblName);
+            this.Name = "ProfileForm";
+            this.Text = "ProfileForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.Label lblOccupation;
+        private System.Windows.Forms.Label lblBirthDate;
+        private System.Windows.Forms.Button btnDone;
+    }
+}
---
+++ TestGUI/ProfileForm.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using HelpDeskClassLibrary;
+
+namespace TestGUI
+{
+    public partial class ProfileForm : Form
+    {
+        User user = new User();
+        public ProfileForm(User user)
+        {
+            InitializeComponent();
+
+            this.user = user;
+
+            FillControls();
+        }
+
+        private void FillControls()
+        {
+            UserProfile userProfile = user.Profile;
+
+            lblName.Text = $"{userProfile.FirstName} {userProfile.LastName}";
+            lblOccupation.Text = $"{userProfile.Occupation}";
+            lblBirthDate.Text = $"{userProfile.Birthday.ToString("yyyy-MM-dd")}";
+        }
+
+        private void btnDone_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
